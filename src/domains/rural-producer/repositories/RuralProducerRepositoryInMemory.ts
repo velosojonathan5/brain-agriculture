5dda42ab732c038af9a386eb68b813fa75a08032
Jonathan Veloso <velosojonathan5@gmail.com>
2024-05-14 23:29:58 -0300

    remove commented code

--- src/domains/rural-producer/repositories/RuralProducerRepositoryInMemory.ts
+++ src/domains/rural-producer/repositories/RuralProducerRepositoryInMemory.ts
@@ -1,7 +1,4 @@
-import { StateEnum } from "../../../dictionary/state.enum";
-import { PlantedCrop } from "../entities/FarmEntity";
 import { RuralProducerEntity } from "../entities/RuralProducer.entity";
-import { RuralProducerSumsEntity } from "../entities/RuralProducerSums.entity";
 
 export class RuralProducerRepositoryInMemory {
     private store: RuralProducerEntity[] = [];
@@ -30,60 +27,4 @@ export class RuralProducerRepositoryInMemory {
     async find(): Promise<RuralProducerEntity[]> {
         return this.store;
     }
-
-    // async getSums(): Promise<RuralProducerSumsEntity> {
-
-    //     let amountFarms = 0;
-    //     let amountHectare = 0;
-    //     const soloUseArea = { vegetation: 0, arable: 0 };
-    //     const mapState: any = {};
-    //     const mapCrop: any = {};
-        
-    //     for(let producer of this.store) {
-    //         amountFarms++;
-
-    //         amountHectare += producer.farm.farmTotalArea;
-
-    //         if(!mapState[producer.farm.state]) {
-    //             mapState[producer.farm.state] = { amountFarms: 0, amountHectare: 0 }
-    //         }
-    //         mapState[producer.farm.state].amountFarms++;
-    //         mapState[producer.farm.state].amountHectare += producer.farm.farmTotalArea;
-
-    //         for(let crop of producer.farm.plantedCrops) {
-    //             if(!mapCrop[crop]) {
-    //                 mapCrop[crop] = { amountFarms: 0, amountHectare: 0 }
-    //             }
-    //             mapCrop[crop].amountFarms++;
-    //             mapCrop[crop].amountHectare += producer.farm.farmTotalArea;
-    //         }
-
-    //         soloUseArea.vegetation += producer.farm.vegetationArea;
-    //         soloUseArea.arable += producer.farm.arableArea;
-    //     }
-
-    //     const amountByState = Object.keys(mapState).map(key => { 
-    //         return {
-    //             state: key as StateEnum,
-    //             amountFarms: mapState[key].amountFarms,
-    //             amountHectare: mapState[key].amountHectare
-    //         }
-    //     });
-
-    //     const amountByCrop = Object.keys(mapCrop).map(key => { 
-    //         return {
-    //             crop: key as PlantedCrop,
-    //             amountFarms: mapCrop[key].amountFarms,
-    //             amountHectare: mapCrop[key].amountHectare
-    //         }
-    //     });
-
-    //     return RuralProducerSumsEntity.create({
-    //         amountFarms,
-    //         amountHectare,
-    //         amountByState,
-    //         amountByCrop,
-    //         soloUseArea,
-    //     });
-    // }
 }
